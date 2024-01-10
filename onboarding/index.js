ef1c5b911b7c42c33defe530809a25eb5ac74389
Angel Rojo Marijuan <rojo@adobe.com>
2024-01-10 10:24:23 +0100

    ACSP-124 - Authentication OAuth - make onboarding use @adobe/aio-lib-ims (#9)
    * add @adobe/aio-lib-ims dependency
    
    * change onboarding to use @adobe/aio-lib-ims lib
    
    * remove unneeded env var
    
    * remove unneeded comment
    
    * extract scopes to give it a name and make it selfdocumented
    
    * make use of already defined environment const
    
    * move things around
    
    * remove unused import

--- onboarding/index.js
+++ onboarding/index.js
@@ -1,56 +1,25 @@
-const fetch = require("node-fetch");
-const registrations = require("./custom/registrations.json");
+const { context, getToken } = require('@adobe/aio-lib-ims')
 require('dotenv').config();
 
-async function getAccessToken(envConfigs) {
-    const scopes = envConfigs.OAUTH_SCOPES;
-    const baseUrl = envConfigs.OAUTH_BASE_URL;
-    const clientId = envConfigs.OAUTH_CLIENT_ID;
-    const clientSecret = envConfigs.OAUTH_CLIENT_SECRET;
-    const generateAccessTokenReq = await fetch(
-        `${baseUrl}v3?client_id=${clientId}`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `client_secret=${clientSecret}&grant_type=client_credentials&scope=${scopes}`
-        }
-    )
-
-
-    const result = await generateAccessTokenReq.json();
-
-    if (!result?.access_token) {
-        console.log(`Unable to generate oauth token: ${result.error}`);
-        return {
-            success: false,
-            error: result.error
-        }
-    }
-    return {
-        success: true,
-        token: result?.access_token
-    }
-}
-
 async function main() {
-    const environment = process.env;
-    const registrations = require('./custom/registrations.json');
-
     console.log('Starting the process of on-boarding based on you registration choice');
 
-    const generateAccessTokenResult = await getAccessToken(environment);
-
-    if (!generateAccessTokenResult?.success) {
-        return {
-            code: 500,
-            error: `Unable to generate oauth token: ${generateAccessTokenResult.error}`
-        };
+    const environment = process.env;
+    const registrations = require('./custom/registrations.json');
+    const ioManagementAPIScopes = ['AdobeID', 'openid', 'read_organizations', 'additional_info.projectedProductContext', 'additional_info.roles', 'adobeio_api', 'read_client_secret', 'manage_client_secrets'];
+    const config = {
+        client_id: environment.OAUTH_CLIENT_ID,
+        client_secrets: [environment.OAUTH_CLIENT_SECRET],
+        technical_account_id: environment.OAUTH_TECHNICAL_ACCOUNT_ID,
+        technical_account_email: environment.OAUTH_TECHNICAL_ACCOUNT_EMAIL,
+        ims_org_id: environment.OAUTH_ORG_ID,
+        scopes: ioManagementAPIScopes
     }
 
-    const accessToken = generateAccessTokenResult.token;
+    await context.setCurrent('onboarding-config')
+    await context.set('onboarding-config', config)
 
+    const accessToken = await getToken()
     const createProvidersResult = await require('./providers').main(registrations, accessToken);
 
     if (!createProvidersResult.success) {
