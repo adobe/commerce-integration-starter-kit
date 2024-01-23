f7077dfe936d51ff8b41a3812e0de4df49db9cbe
Angel Rojo Marijuan <rojo@adobe.com>
2024-01-23 11:54:33 +0100

    INTERNAL - fix broken onboarding (#25)

--- onboarding/index.js
+++ onboarding/index.js
@@ -18,14 +18,14 @@ require('dotenv').config();
 async function main() {
     console.log('Starting the process of on-boarding based on you registration choice');
 
-    const environment = process.env;
+    const registrations = require('./custom/registrations.json');
     const ioManagementAPIScopes = ['AdobeID', 'openid', 'read_organizations', 'additional_info.projectedProductContext', 'additional_info.roles', 'adobeio_api', 'read_client_secret', 'manage_client_secrets'];
     const config = {
-        client_id: environment.OAUTH_CLIENT_ID,
-        client_secrets: [environment.OAUTH_CLIENT_SECRET],
-        technical_account_id: environment.OAUTH_TECHNICAL_ACCOUNT_ID,
-        technical_account_email: environment.OAUTH_TECHNICAL_ACCOUNT_EMAIL,
-        ims_org_id: environment.OAUTH_ORG_ID,
+        client_id: process.env.OAUTH_CLIENT_ID,
+        client_secrets: [process.env.OAUTH_CLIENT_SECRET],
+        technical_account_id: process.env.OAUTH_TECHNICAL_ACCOUNT_ID,
+        technical_account_email: process.env.OAUTH_TECHNICAL_ACCOUNT_EMAIL,
+        ims_org_id: process.env.OAUTH_ORG_ID,
         scopes: ioManagementAPIScopes
     }
 
@@ -33,7 +33,7 @@ async function main() {
     await context.set('onboarding-config', config)
 
     const accessToken = await getToken()
-    const createProvidersResult = await require('./providers').main(registrations, environment, accessToken);
+    const createProvidersResult = await require('./providers').main(registrations, process.env, accessToken);
 
     if (!createProvidersResult.success) {
         const errorMessage = `Process of on-boarding (providers) failed with error: ${createProvidersResult.error}`;
@@ -46,7 +46,7 @@ async function main() {
     }
 
     const providers = createProvidersResult.result;
-    const createProvidersMetadataResult = await require('./metadata').main(registrations, providers, environment, accessToken);
+    const createProvidersMetadataResult = await require('./metadata').main(registrations, providers, process.env, accessToken);
 
     if (!createProvidersMetadataResult.success) {
         const errorMessage = `Process of on-boarding (metadata) failed with error: ${createProvidersResult.error}`;
@@ -58,7 +58,7 @@ async function main() {
         }
     }
 
-    const registerEntityEventsResult = await require('./registrations').main(registrations, providers, environment, accessToken);
+    const registerEntityEventsResult = await require('./registrations').main(registrations, providers, process.env, accessToken);
     if (!registerEntityEventsResult.success) {
         const errorMessage = `Process of on-boarding (registrations) failed with error: ${createProvidersResult.error}`;
         console.log(errorMessage);
