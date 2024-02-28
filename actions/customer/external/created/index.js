256d1888a92c20006ecd3509331c95bbecf37b71
Angel Rojo Marijuan <rojo@adobe.com>
2024-02-28 13:09:37 +0100

    ACSP-177 - Consolidate senData - customer external created (#60)
    * customer external created
    
    * remove unused declarations
    
    * improve logger message

--- actions/customer/external/created/index.js
+++ actions/customer/external/created/index.js
@@ -50,6 +50,9 @@ async function main (params) {
 
     logger.debug(`[Customer][External][Created] Start sending data: ${JSON.stringify(transformed)}`)
     const result = await sendData(params, transformed, preProcessed)
+    if (!result.success) {
+      return actionErrorResponse(result.statusCode, result.message)
+    }
 
     logger.debug(`[Customer][External][Created] Postprocess data: ${JSON.stringify(params)}`)
     const postProcessed = postProcess(params, transformed, preProcessed, result)
@@ -58,7 +61,7 @@ async function main (params) {
     return actionSuccessResponse('Customer created successfully')
   } catch (error) {
     logger.error(`[Customer][External][Created] Error processing the request: ${error}`)
-    return actionErrorResponse(error.response?.statusCode || HTTP_INTERNAL_ERROR, error.message)
+    return actionErrorResponse(HTTP_INTERNAL_ERROR, error)
   }
 }
 
