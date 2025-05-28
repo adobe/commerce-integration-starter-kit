328cd65392c5eeae1c9bce42d688d1cc69cb6d1c
Ivan Porto <iporto@adobe.com>
2025-05-28 15:42:48 +0200

    chore: configure telemetry and metrics

---
+++ actions/customer/commerce/metrics.js
@@ -0,0 +1,17 @@
+const { defineMetrics } = require('@adobe/aio-sk-lib-telemetry')
+const { ValueType } = require('@adobe/aio-sk-lib-telemetry/otel-api')
+
+/** Metrics used across all actions. */
+const commerceCustomerMetrics = defineMetrics((meter) => {
+  return {
+    consumerSuccessCounter: meter.createCounter(
+      'customer.commerce.consumer.success_count', {
+        description: 'A counter for the number of successful Commerce Consumer actions.',
+        valueType: ValueType.INT
+      })
+  }
+})
+
+module.exports = {
+  commerceCustomerMetrics
+}
--- actions/telemetry.js
+++ actions/telemetry.js
@@ -1,17 +1,26 @@
-const { defineTelemetryConfig, makeNodeSdkConfig } = require('@adobe/aio-lib-telemetry');
+const {
+  defineTelemetryConfig,
+  getAioRuntimeResource,
+  getPresetInstrumentations
+} = require('@adobe/aio-sk-lib-telemetry')
 
 /** The telemetry configuration used across all actions */
-const telemetryConfig = defineTelemetryConfig((params) => {
-  const sdkConfig = makeNodeSdkConfig({
-    instrumentations: "full",
-    serviceName: 'commerce-integration-starter-kit',
-    serviceVersion: '1.0.0',
-  })
-
+const telemetryConfig = defineTelemetryConfig((params, isDev) => {
   return {
-    sdkConfig,
+    sdkConfig: {
+      serviceName: 'commerce-integration-starter-kit',
+      resource: getAioRuntimeResource(),
+      instrumentations: getPresetInstrumentations('simple')
+
+      // Configure where to export the telemetry signals.
+      // Use `params` if you need to access some secret values.
+      // spanProcessors: [],
+      // logRecordProcessors: [],
+      // metricReader: null
+    },
+
     diagnostics: {
-      logLevel: 'debug'
+      logLevel: isDev ? 'debug' : 'error'
     }
   }
 })
