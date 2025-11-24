f9e116cc88bd2fd33dcea84638d36b48db7c5c8b
Ivan Porto <iporto@adobe.com>
2025-11-24 18:56:17 +0100

    refactor: regenerate via init script

--- extensibility.config.js
+++ extensibility.config.js
@@ -9,26 +9,11 @@ module.exports = {
           { label: "Option 1", value: "option1" },
           { label: "Option 2", value: "option2" },
         ],
+        selectionMode: "single",
         default: "option1",
         description: "This is a description for the example list",
       },
-      {
-        name: "currency",
-        type: "text",
-        label: "Currency",
-      },
-      {
-        name: "paymentMethod",
-        type: "text",
-        label: "Payment Test Method",
-      },
-      {
-        name: "testField",
-        type: "text",
-        description: "This is a description for the test field",
-        label: "Test Field",
-        default: "Test Default Value1",
-      },
+      { name: "currency", type: "text", label: "Currency" },
     ],
   },
 };
